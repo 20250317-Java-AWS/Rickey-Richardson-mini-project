f16d55b7bd23b1e3b0d0c97fe2dc1ee31e6d94f6
Rickey Richardson <rickeyrichardson@Rickeys-MacBook-Pro.local>
2025-03-19 22:25:45 -0400

    added components for baseball app

--- mini-project/src/app/app.component.ts
+++ mini-project/src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DivisionsComponent } from './divisions/divisions.component';
+import { LeaguesComponent } from './leagues/leagues.component';
+import { TeamsComponent } from './teams/teams.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [DivisionsComponent, LeaguesComponent, TeamsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'mini-project';
+  constructor() {
+    console.log("The Baseball App Component is running!")
+  }
 }
